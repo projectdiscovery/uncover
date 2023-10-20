69ee45e46a5b0dec6f3671f6c8a82c976314f782
sandeep <8293321+ehsandeep@users.noreply.github.com>
2023-10-20 17:44:10 +0530

    version update

--- runner/banners.go
+++ runner/banners.go
@@ -13,7 +13,7 @@ const banner = `
 `
 
 // Version is the current version of uncover
-const version = `v1.0.6`
+const version = `v1.0.7`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
