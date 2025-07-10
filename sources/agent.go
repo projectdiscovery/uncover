c9d10fe51d578ead78206ddc55f9fc4cb2e396e5
TonyD0g <85001557+TonyD0g@users.noreply.github.com>
2025-07-10 14:04:41 +0800

    Update agent.go

--- sources/agent.go
+++ sources/agent.go
@@ -1,9 +1,14 @@
 package sources
 
 type Query struct {
-	Query string
-	Limit int
-	Full  bool
+	Query      string
+	Limit      int
+	Full       bool   // Used for fofa query of full data
+	StatusCode string // Used for hunter to query the content of a specified status code list
+	PortFilter bool   // Used for Hunter data filtering
+	IsWeb      int    // Used for Hunter filtering asset types
+	StartTime  string // Used for hunter filtering start time
+	EndTime    string // Used for hunter filtering end time
 }
 
 type Agent interface {
