b948fe71cc6d3ccfa90e8672a940c18dbf72912b
Yousef <y9206345@gmail.com>
2026-02-18 17:25:51 +0300

    feat: add InfoAgent interface for balance checking

--- sources/agent.go
+++ sources/agent.go
@@ -5,7 +5,14 @@ type Query struct {
 	Limit int
 }
 
+// Agent is the basic interface for all search engines
 type Agent interface {
 	Query(*Session, *Query) (chan Result, error)
 	Name() string
+}
+
+// InfoAgent is an interface for agents that support checking balance/account info
+type InfoAgent interface {
+	Agent
+	Info(*Session) (string, error)
 }
